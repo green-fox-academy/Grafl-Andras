03e22a2d711340a2a0289f555d1b685cf2b3bdc5
Grafl Andras <grand.golyamail@gmail.com>
2018-10-29 11:23:31 +0100

    define-basic-info.ts added

---
+++ week02/day01/define-basic-info.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+let myName: string = "Gräfl András";
+let myAge: number = 27;
+let myHeight: number = 1.85;
+let marriage: boolean = false;
+
+console.log("My name is " + myName);
+console.log("My age is " + myAge);
+console.log("My height is " + myHeight + " m");
+console.log("Am I married? " + marriage);
