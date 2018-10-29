7d28628abad57ca7debe211ebf3df0276e49bd94
Grafl Andras <grand.golyamail@gmail.com>
2018-10-29 14:49:09 +0100

    Added multiplication-table.ts

---
+++ week02/day01/multiplication-table.ts
@@ -0,0 +1,7 @@
+'use strict';
+let number: number = 15;
+let szorzo: number = 1;
+
+for (szorzo = 1; szorzo <= 10; szorzo++) {
+    console.log(szorzo + " * " + number + " = " + (szorzo*number));
+}
