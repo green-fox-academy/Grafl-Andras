1294f1d33be5f9f6e2bf0953064dadf2f5e1abda
Grafl Andras <grand.golyamail@gmail.com>
2018-10-29 10:57:56 +0100

    coding-hours.ts added

---
+++ week02/day01/coding-hours.ts
@@ -0,0 +1,3 @@
+'use strict';
+console.log("Print how many hours is spent with coding in a semester by an attendee, if the attendee only codes on workdays: " + (17 * 5 * 6));
+console.log("Print the percentage of the coding hours in the semester if the average work hours weekly is 52: " + ((17 * 5 * 6) / (17 * 52 / 100)));
