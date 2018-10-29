d50bde0fb7dda576292ab27b8b84931418988b08
Grafl Andras <grand.golyamail@gmail.com>
2018-10-29 15:26:06 +0100

    Added draw-triangle.ts

---
+++ week02/day01/draw-triangle.ts
@@ -0,0 +1,18 @@
+'use strict';
+export {}
+// Write a program that draws a triangle like this:
+//
+// *
+// **
+// ***
+// ****
+//
+// The triangle should have as many lines as lineCount is
+let lineCount: number = 4;
+let piramis: string = "*";
+let i: number = null;
+
+for (i = 0; i < lineCount; i++) {
+    console.log(piramis);
+    piramis += "*";
+}
