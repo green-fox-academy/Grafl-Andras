8f2a12254394d812950176c5ed46be9c066683e9
Grafl Andras <grand.golyamail@gmail.com>
2018-10-29 12:15:02 +0100

    cuboid.ts added

---
+++ week02/day01/cuboid.ts
@@ -0,0 +1,7 @@
+'use strict';
+let cuboidLength: number = 8;
+let cuboidWidth: number = 6;
+let cuboidHeight: number = 5;
+
+console.log("Surface Area: " + (2*(cuboidLength * cuboidWidth + cuboidWidth * cuboidHeight + cuboidHeight * cuboidLength)));
+console.log("Volume: " + (cuboidLength * cuboidWidth * cuboidHeight));
