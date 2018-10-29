af691b0f6afa48c85217c684f4c1ac577850d588
Grafl Andras <grand.golyamail@gmail.com>
2018-10-29 15:16:39 +0100

    Added fizz-buzz.ts

---
+++ week02/day01/fizz-buzz.ts
@@ -0,0 +1,22 @@
+'use-strict';
+let i: number = 1;
+
+for (i = 1; i <= 100; i++){
+
+    if ((i % 3) !== 0 && (i % 5) !== 0) {
+
+        console.log(i);
+
+    } else if ((i % 3) == 0 && (i % 5) == 0) {
+        
+        console.log("FizzBuzz");
+    
+    } else if ((i % 5) == 0) {
+
+        console.log("Buzz");
+
+    } else if ((i % 3) == 0) {
+        console.log("Fizz");
+
+    }
+}
