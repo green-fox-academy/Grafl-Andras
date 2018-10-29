bd8e27dfa80879d9ae1a73e34fefb24e5f36c42a
Grafl Andras <grand.golyamail@gmail.com>
2018-10-29 10:47:16 +0100

    Added two-numbers.ts

---
+++ week02/day01/two-numbers.ts
@@ -0,0 +1,10 @@
+'use strict';
+let a = 22;
+let b = 13;
+
+console.log("Print the result of 13 added to 22: " + (b + a));
+console.log("Print the result of 13 substracted from 22: " + (a - b));
+console.log("Print the result of 22 multiplied by 13: " + (a * b));
+console.log("Print the result of 22 divided by 13 (as a decimal fraction): " + (a / b));
+console.log("Print the remainder of 22 divided by 13: " + (a % b));
+
