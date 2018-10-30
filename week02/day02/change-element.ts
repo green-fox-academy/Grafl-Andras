16738c6793b2c475315fe766eb15433f172a8ead
Grafl Andras <grand.golyamail@gmail.com>
2018-10-30 13:29:46 +0100

    Added change-element.ts

---
+++ week02/day02/change-element.ts
@@ -0,0 +1,10 @@
+'use strict';
+export{}
+
+let numlist: number[]= [1, 2, 3, 8, 5, 6];
+let changedArr: number[]=numlist.map(function (change) {
+    if (change === 8) {
+        return 4;
+    } return change;
+});
+console.log(changedArr);
