9c4457f9abeb070fef895c6cc540992fe5b23a6a
Grafl Andras <grand.golyamail@gmail.com>
2018-11-07 15:20:07 +0100

    Changed parameters

--- week03/day03/10-rainbow-boxes/rainbow-box-function-ts.ts
+++ week03/day03/10-rainbow-boxes/rainbow-box-function-ts.ts
@@ -5,14 +5,13 @@ const ctx = canvas.getContext('2d');
 
 // DO NOT TOUCH THE CODE ABOVE THIS LINE
 
-function drawRectangles(h, c ) {
+function drawRectangles(h, c) {
     ctx.fillRect((canvas.width / 2) - (h / 2), (canvas.height / 2) - (h / 2), h, h);
     ctx.fillStyle = `rgb(${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)}, ${Math.floor(Math.random()*255)}`;
 }
 
 for (let i: number = 30; i > 0; i--){
-    drawRectangles((13 * i), 7 * i);
-    
+    drawRectangles((13 * i), 7 * i);    
 }
 // Create a square drawing function that takes 2 parameters:
 // The square size, and the fill color,
