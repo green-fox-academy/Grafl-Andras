781eb6d55e278f8aa55c2d990da86c31b37f5435
Grafl Andras <grand.golyamail@gmail.com>
2018-11-08 09:31:18 +0100

    changed the loop's runtume

--- week03/day03/16-starry-night/starry-night-ts.ts
+++ week03/day03/16-starry-night/starry-night-ts.ts
@@ -14,9 +14,9 @@ const ctx = canvas.getContext('2d');
 ctx.fillStyle = 'black';
 ctx.fillRect(0, 0, 600, 400);
 
-for (let i: number = 0; i < 40; i++){
+for (let i: number = 0; i < 900; i++){
     let randomGrey = Math.floor(Math.random()*255);
     ctx.fillStyle = `rgb(${randomGrey}, ${randomGrey}, ${randomGrey})`;
-    ctx.fillRect(Math.floor(Math.random()*600), Math.floor(Math.random()*400), 20, 20);
+    ctx.fillRect(Math.floor(Math.random()*600), Math.floor(Math.random()*400), 2, 2);
 }
 
