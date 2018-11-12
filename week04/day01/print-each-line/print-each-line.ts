1ab2145217c69b4ea3139684f51ddb26c6aff30d
Grafl Andras <grand.golyamail@gmail.com>
2018-11-12 10:30:05 +0100

    Added print-eachh-line.ts

--- week04/day01/print-each-line/print-each-line.ts
+++ week04/day01/print-each-line/print-each-line.ts
@@ -1,5 +1,10 @@
 'use strict';
 
+// Write a program that opens a file called 'my-file.txt', then prints
+// each of lines form the file.
+// If the program is unable to read the file (for example it does not exists),
+// then it should print an error message like: 'Unable to read file: my-file.txt'
+
 const fs = require('fs');
 
 try {
