b6a127074ce2d284b768ee7fac3becf19e0a6395
Grafl Andras <grand.golyamail@gmail.com>
2018-11-12 10:29:21 +0100

    Added print-each-line.ts

---
+++ week04/day01/print-each-line/print-each-line.ts
@@ -0,0 +1,10 @@
+'use strict';
+
+const fs = require('fs');
+
+try {
+    let fileContent = fs.readFileSync('my-file.txt', 'utf-8');
+    console.log(fileContent);
+} catch (error) {
+    console.log("Unable to read file: my-file.txt.");
+}
