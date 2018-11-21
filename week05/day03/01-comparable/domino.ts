5938c331f149e35dfbb9c0270ba760bfc168b077
Grafl Andras <grand.golyamail@gmail.com>
2018-11-21 11:22:44 +0100

    feat(domino): domino.ts added

---
+++ week05/day03/01-comparable/domino.ts
@@ -0,0 +1,19 @@
+import {Comparable} from './compare';
+
+class Domino implements Comparable{
+    values: number[];
+    constructor(valueA: number, valueB: number) {
+        this.values = [valueA, valueB];
+    }
+    compareTo(other: Domino): number{
+        if (this.values[0] < other.values[0]){
+            return -1;
+        } else if (this.values[0] === other.values[0]){
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+}
+export {Domino};
+
