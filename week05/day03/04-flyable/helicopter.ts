4142028d2429d6f02254f02edf1ecf323fedec32
Grafl Andras <grand.golyamail@gmail.com>
2018-11-21 15:26:22 +0100

    fix(flyable): added some consolelogs to helicopter.ts

--- week05/day03/04-flyable/helicopter.ts
+++ week05/day03/04-flyable/helicopter.ts
@@ -3,7 +3,17 @@ import {Vehicle} from './vehicle';
 'use strict';
 
 export class Helicopter extends Vehicle implements Flyable{
-    land(){};
-    fly(){};
-    takeOff(){};
+    land(){
+        console.log(`${this.constructor.name} landed.`);
+    };
+    fly(){
+        console.log(`${this.constructor.name} is flying.`);
+    };
+    takeOff(){
+        console.log(`${this.constructor.name} taken off`);
+    };
 }
+
+let ironBird = new Helicopter('Iron Bird', 'Mercedes', 'Gasoline');
+
+ironBird.takeOff();
