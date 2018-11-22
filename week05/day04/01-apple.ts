861c5ba3249f426c415b38b6bb6dade8f283ce87
Grafl Andras <grand.golyamail@gmail.com>
2018-11-22 11:29:42 +0100

    feat(apple): apple.ts added

---
+++ week05/day04/01-apple.ts
@@ -0,0 +1,12 @@
+'use strict';
+
+export class Apple {
+    string: string;
+
+    constructor(string: string){
+        this.string = string;
+    }
+    getString(){
+        return this.string;
+    }
+}
