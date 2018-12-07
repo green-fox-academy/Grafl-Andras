931b7b16bea0a91f24e4511e7eb5e85040101521
Grafl Andras <grand.golyamail@gmail.com>
2018-12-07 16:50:50 +0100

    final added

--- week07/day05/gallery.js
+++ week07/day05/gallery.js
@@ -1,47 +1,77 @@
 let myPics = [
-    'https://www.farmersalmanac.com/wp-content/uploads/2012/07/Supercell-i811649298-600x319.jpg',
-    'https://i.pinimg.com/originals/be/45/9d/be459dfa70d75f8103ebeaae11e05010.jpg',
-    'https://www.desktopbackground.org/download/o/2012/12/05/494526_supercell-clouds-wallpaper_1920x1080_h.jpg',
-    'https://fournews-assets-prod-s3b-ew1-aws-c4-pml.s3.amazonaws.com/media/2017/12/snow_london_g_hd.jpg',
-    'https://www.autonavigator.hu/wp-content/uploads/2018/01/223532_source.jpg',
-    'http://openwalls.com/image/31592/cloudy_wheather_1920x1080.jpg',
-    'http://www.spinus.net/nature/mammals-ungulates/guanaco-01/2015-02-n027269-guanaco-spinus-s.jpg',
-    'http://www.cartoonswallpapers.net/wallpapers/lion-king-wallpaper-macbook.jpg'
+  { 'url': 'https://www.farmersalmanac.com/wp-content/uploads/2012/07/Supercell-i811649298-600x319.jpg', 'title': 'Title1', 'desc': 'Valószínű, hogy a korhes szárs tálázásán sujas lesz majd létlendő bolyagvás. Mindeme idradások mellett el kell csörgetnie, törmenző áros énevő viszelt.' },
+  { 'url': 'https://i.pinimg.com/originals/be/45/9d/be459dfa70d75f8103ebeaae11e05010.jpg', 'title': 'Title2', 'desc': 'Hallódja el ezt a gyetést rektájának, szembánának, szolvászának. Zabulja ki az edzést, majd csegetse meg a „gyetés zsombuta” zülest!' },
+  { 'url': 'https://www.desktopbackground.org/download/o/2012/12/05/494526_supercell-clouds-wallpaper_1920x1080_h.jpg', 'title': 'Title3', 'desc': 'A káng gödés bélyezelő kolták kodája hetnyi. A fézség szályzáját gyatatós orsaságok egyelhelő piterzások óta lódnak, néha a bolyára zsiráztak, máskor a „panya alatt számítottak”.' },
+  { 'url': 'https://fournews-assets-prod-s3b-ew1-aws-c4-pml.s3.amazonaws.com/media/2017/12/snow_london_g_hd.jpg', 'title': 'Title4', 'desc': 'Most, az ehely feletézésre segő dakozása, a szorlan atás és a vezés újból dipadhadóvá – sapánok szerint ezúttal sujassá – pillagolta, hogy a fézség a dalmas vintésével és a tenges hinaságával együtt hajontson.' },
+  { 'url': 'https://www.autonavigator.hu/wp-content/uploads/2018/01/223532_source.jpg', 'title': 'Title5', 'desc': 'A pamon szítányokat bárki pártankodhatja a főzésében. Értés svés másnéven vedék borcos nyegőzésök, melyekkel kodoltokat lehet teregnie.A pamon szítányokat bárki pártankodhatja a főzésében. Értés svés másnéven vedék borcos nyegőzésök, melyekkel kodoltokat lehet teregnie.A pamon szítányokat bárki pártankodhatja a főzésében. Értés svés másnéven vedék borcos nyegőzésök, melyekkel kodoltokat lehet teregnie.A pamon szítányokat bárki pártankodhatja a főzésében. Értés svés másnéven vedék borcos nyegőzésök, melyekkel kodoltokat lehet teregnie.' },
+  { 'url': 'http://openwalls.com/image/31592/cloudy_wheather_1920x1080.jpg', 'title': 'Title6', 'desc': 'Például az alék ellemzése iség, málás, a kölöntés ellemzése sziva. A matlakt vedék halan fogálája römnyik a főzés vercénél.' },
+  { 'url': 'http://www.spinus.net/nature/mammals-ungulates/guanaco-01/2015-02-n027269-guanaco-spinus-s.jpg', 'title': 'Title7', 'desc': 'Egy főzésben kedékeket is meg lehet nászkogatnia, de ezt csak lentő nyuca betítőjével. A nertárához egy sajas, az aláson mező kedéket kell furulnia, például: revég.' },
+  { 'url': 'http://www.cartoonswallpapers.net/wallpapers/lion-king-wallpaper-macbook.jpg', 'title': 'Title8', 'desc': 'A saját porcon törben kedékeket nem lehet nyalódznia (kivéve ha fejedik egy pítány a porcon, sem a fékező fesüvereken törben kedékeket (például igonyokban (nyedés, csőztet), vagy matrással fékező fesüvereken), stb.' }
 ]
 
 let footerImgs = document.querySelectorAll('img.footerimage');
-console.log(footerImgs);
+let imgCounter = 0;
 
 for (let i = 0; i < footerImgs.length; i++) {
-    footerImgs[i].setAttribute('src', `${myPics[i]}`);
+  footerImgs[i].setAttribute('src', `${myPics[i].url}`);
 }
 
 let mainPic = document.querySelector('img.mainPic');
-mainPic.setAttribute('src', `${myPics[0]}`);
+mainPic.setAttribute('src', `${myPics[imgCounter].url}`);
+
+
 
 let goRight = document.querySelector('button.right');
-let imgCounter = 0;
+
 goRight.onclick = () => {
 
-    if (imgCounter <= myPics.length - 1) {
-        if (imgCounter === myPics.length - 1) {
-            imgCounter = 0;
-            mainPic.setAttribute('src', `${myPics[imgCounter]}`);
-        } else {
-            imgCounter++;
-            mainPic.setAttribute('src', `${myPics[imgCounter]}`);
-        };
-    }
+  if (imgCounter <= myPics.length - 1) {
+    if (imgCounter === myPics.length - 1) {
+      imgCounter = 0;
+      mainPic.setAttribute('src', `${myPics[imgCounter].url}`);
+      h2.innerText = `${myPics[imgCounter].title}`;
+      pTag.innerText = `${myPics[imgCounter].desc}`;
+    } else {
+      imgCounter++;
+      mainPic.setAttribute('src', `${myPics[imgCounter].url}`);
+      h2.innerText = `${myPics[imgCounter].title}`;
+      pTag.innerText = `${myPics[imgCounter].desc}`;
+    };
+  }
 };
 let goLeft = document.querySelector('button.left');
 goLeft.onclick = () => {
-    if (imgCounter >= 0) {
-        if (imgCounter === 0) {
-            imgCounter = myPics.length - 1;
-            mainPic.setAttribute('src', `${myPics[imgCounter]}`);
-        } else {
-            imgCounter--;
-            mainPic.setAttribute('src', `${myPics[imgCounter]}`);
-        };
-    }
+  if (imgCounter >= 0) {
+    if (imgCounter === 0) {
+      imgCounter = myPics.length - 1;
+      mainPic.setAttribute('src', `${myPics[imgCounter].url}`);
+      h2.innerText = `${myPics[imgCounter].title}`;
+      pTag.innerText = `${myPics[imgCounter].desc}`;
+    } else {
+      imgCounter--;
+      mainPic.setAttribute('src', `${myPics[imgCounter].url}`);
+      h2.innerText = `${myPics[imgCounter].title}`;
+      pTag.innerText = `${myPics[imgCounter].desc}`;
+    };
+  }
 };
+
+let footerButtons = document.querySelectorAll('button.footer');
+let h2 = document.querySelector('h2');
+let pTag = document.querySelector('p');
+console.log(pTag);
+footerButtons.forEach((e, i) => {
+  e.onclick = () => {
+    mainPic.setAttribute('src', `${myPics[i].url}`);
+    imgCounter = i;
+    h2.innerText = `${myPics[i].title}`;
+    pTag.innerText = `${myPics[i].desc}`;
+  }
+});
+
+
+h2.innerText = `${myPics[imgCounter].title}`;
+pTag.innerText = `${myPics[imgCounter].desc}`;
+
+
+
