49341a7147efc57af4489dbd7932545b84c7ecce
Grafl Andras <grand.golyamail@gmail.com>
2018-12-18 11:45:56 +0100

    added comments and database

--- week09/day02/exercise1.sql
+++ week09/day02/exercise1.sql
@@ -1,4 +1,4 @@
---https://lagunita.stanford.edu/courses/DB/SQL/SelfPaced/courseware/ch-sql/seq-exercise-sql_movie_query_core/
+/* https://lagunita.stanford.edu/courses/DB/SQL/SelfPaced/courseware/ch-sql/seq-exercise-sql_movie_query_core/ */
 
 /* Create the schema for our tables */
 create table Movie(mID int, title text, year int, director text);
@@ -39,21 +39,21 @@ insert into Rating values(206, 106, 5, '2011-01-19');
 insert into Rating values(207, 107, 5, '2011-01-20');
 insert into Rating values(208, 104, 3, '2011-01-02');
 
--- 1. Find the titles of all movies directed by Steven Spielberg.
+/* 1. Find the titles of all movies directed by Steven Spielberg. */
 
 SELECT title
 FROM Movie
 WHERE director = 'Steven Spielberg';
 
 
--- 2. Find all years that have a movie that received a rating of 4 or 5, and sort them in increasing order.
+/* 2. Find all years that have a movie that received a rating of 4 or 5, and sort them in increasing order. */
 
 SELECT DISTINCT year
 FROM Movie, Rating
 WHERE Movie.mId = Rating.mId AND stars IN (4, 5)
 ORDER BY year;
 
--- VAGY
+/* VAGY */
 
 SELECT DISTINCT year
 FROM Movie
@@ -61,7 +61,7 @@ INNER JOIN Rating ON Movie.mId = Rating.mId
 WHERE stars IN (4, 5)
 ORDER BY year;
 
--- VAGY
+/* VAGY */
 
 SELECT DISTINCT year
 FROM Movie
@@ -69,7 +69,7 @@ INNER JOIN Rating USING(mId)
 WHERE stars IN (4, 5)
 ORDER BY year;
 
--- VAGY
+/* VAGY */
 
 SELECT DISTINCT year
 FROM Movie NATURAL JOIN Rating
@@ -77,14 +77,14 @@ WHERE stars IN (4, 5)
 ORDER BY year;
 
 
--- 3. Find the titles of all movies that have no ratings. 
+/* 3. Find the titles of all movies that have no ratings. */
 
 SELECT title
 FROM Movie
 WHERE mId NOT IN (SELECT mID FROM Rating);
 
 
--- 4. Some reviewers didn't provide a date with their rating. Find the names of all reviewers who have ratings with a NULL value for the date. 
+/* 4. Some reviewers didn't provide a date with their rating. Find the names of all reviewers who have ratings with a NULL value for the date. */
 
 SELECT name
 FROM Reviewer
@@ -92,14 +92,14 @@ INNER JOIN Rating USING(rId)
 WHERE ratingDate IS NULL;
 
 
--- 5. Write a query to return the ratings data in a more readable format: reviewer name, movie title, stars, and ratingDate. Also, sort the data, first by reviewer name, then by movie title, and lastly by number of stars. 
+/* 5. Write a query to return the ratings data in a more readable format: reviewer name, movie title, stars, and ratingDate. Also, sort the data, first by reviewer name, then by movie title, and lastly by number of stars. */
 
 SELECT name, title, stars, ratingDate
 FROM Movie, Rating, Reviewer
 WHERE Movie.mId = Rating.mId AND Reviewer.rId = Rating.rId
 ORDER BY name, title, stars;
 
--- VAGY
+/* VAGY */
 
 SELECT name, title, stars, ratingDate
 FROM Movie
@@ -107,7 +107,7 @@ INNER JOIN Rating ON Movie.mId = Rating.mId
 INNER JOIN Reviewer ON Reviewer.rId = Rating.rId
 ORDER BY name, title, stars;
 
--- VAGY
+/* VAGY */
 
 SELECT name, title, stars, ratingDate
 FROM Movie
@@ -115,14 +115,14 @@ INNER JOIN Rating USING(mId)
 INNER JOIN Reviewer USING(rId)
 ORDER BY name, title, stars;
 
--- VAGY
+/* VAGY */
 
 SELECT name, title, stars, ratingDate
 FROM Movie NATURAL JOIN Rating NATURAL JOIN Reviewer
 ORDER BY name, title, stars;
 
 
--- 6. For all cases where the same reviewer rated the same movie twice and gave it a higher rating the second time, return the reviewer's name and the title of the movie. 
+/* 6. For all cases where the same reviewer rated the same movie twice and gave it a higher rating the second time, return the reviewer's name and the title of the movie. */
 
 SELECT name, title
 FROM Movie
@@ -139,7 +139,7 @@ INNER JOIN Reviewer USING(rId)
 WHERE R1.ratingDate < R2.ratingDate AND R1.stars < R2.stars;
 
 
--- 7. For each movie that has at least one rating, find the highest number of stars that movie received. Return the movie title and number of stars. Sort by movie title. 
+/* 7. For each movie that has at least one rating, find the highest number of stars that movie received. Return the movie title and number of stars. Sort by movie title. */
 
 SELECT title, MAX(stars)
 FROM Movie
@@ -148,7 +148,7 @@ GROUP BY mId
 ORDER BY title;
 
 
--- 8. For each movie, return the title and the 'rating spread', that is, the difference between highest and lowest ratings given to that movie. Sort by rating spread from highest to lowest, then by movie title. 
+/* 8. For each movie, return the title and the 'rating spread', that is, the difference between highest and lowest ratings given to that movie. Sort by rating spread from highest to lowest, then by movie title. */
 
 SELECT title, (MAX(stars) - MIN(stars)) AS rating_spread
 FROM Movie
@@ -157,7 +157,7 @@ GROUP BY mId
 ORDER BY rating_spread DESC, title;
 
 
--- 9. Find the difference between the average rating of movies released before 1980 and the average rating of movies released after 1980. (Make sure to calculate the average rating for each movie, then the average of those averages for movies before 1980 and movies after. Don't just calculate the overall average rating before and after 1980.) 
+/* 9. Find the difference between the average rating of movies released before 1980 and the average rating of movies released after 1980. (Make sure to calculate the average rating for each movie, then the average of those averages for movies before 1980 and movies after. Don't just calculate the overall average rating before and after 1980.) */
 
 SELECT AVG(Before1980.avg) - AVG(After1980.avg)
 FROM (
