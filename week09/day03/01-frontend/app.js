2149790e1d6d32f01f8f2297fd167fc9f716e608
Grafl Andras <grand.golyamail@gmail.com>
2018-12-19 15:10:19 +0100

    done

--- week09/day03/01-frontend/app.js
+++ week09/day03/01-frontend/app.js
@@ -5,6 +5,8 @@ const app = express();
 const path = require('path');
 
 app.use('/assets', express.static('assets'));
+const bp = require('body-parser');
+app.use(bp());
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
@@ -49,17 +51,58 @@ app.get('/greeter', (req, res) => {
 });
 
 // https://github.com/green-fox-academy/teaching-materials/blob/master/workshop/rest-backend/exercises/append-a/README.md
-app.get('/appenda/:appendto', (req, res) => {
-  const { appendto } = req.params;
-  if (appendto) {
-    res.json({ 'appended': `${appendto}a` });
+app.get('/appenda/:appendable', (req, res) => {
+  let word = req.params.appendable;
+  if (word) {
+    res.json({
+      "appended": `${word}a`
+    });
   }
 });
 
 app.get('/appenda', (req, res) => {
-  res.status(404).send();
+  res.status(404);
+  res.send();
 });
 
+
+// https://github.com/green-fox-academy/teaching-materials/blob/master/workshop/rest-backend/exercises/do-until/README.md
+// bodyParsert telepíteni kell 
+// (npm install body-parser --save) 
+// const bp = require('body-parser');
+// app.use(bp());
+const factorio = (number) => {
+  if (number <= 1) {
+    return 1;
+  } else {
+    return number * factorio(number - 1);
+  }
+}
+
+const sum = (number) => {
+  if (number <= 0) {
+    return 0;
+  } else {
+    return number + sum(number - 1);
+  }
+}
+
+app.post('/dountil/:action', (req, res) => {
+  const { action } = req.params;
+  if (action == 'sum') {
+    res.send({
+      "result": sum(req.body.until)
+    });
+  } else if (action == 'factor') {
+    res.send({
+      "result": factorio(req.body.until)
+    });
+  } else {
+    res.send({
+      "error": "Please provide a number!"
+    });
+  }
+});
 
 
 app.listen(8080);
